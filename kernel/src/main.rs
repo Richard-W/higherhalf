027317022db976f82732ca218c7f16ca3cdad7bf
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2019-03-06 18:09:17 +0100

    Unmap the first GiB before printing in kernel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
 extern crate common;
 use common::*;
+
+extern crate mmu;
 
 mod console;
 use console::*;
@@ -15,8 +18,29 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
 }
 
 #[no_mangle]
-fn kmain(framebuffer: &'static Framebuffer) {
-    let mut framebuffer = framebuffer.clone();
+fn kmain(framebuffer: *const Framebuffer) {
+    // Save a copy of the framebuffer.
+    let mut framebuffer = unsafe { (*framebuffer).clone() };
+
+    // Unmap the first GiB.
+    unsafe {
+        // Get the page table.
+        let mut pt4_ptr: *mut mmu::amd64::PageTable;
+        asm!("movq %cr3, $0"
+             : "=r"(pt4_ptr)
+             :
+             :
+        );
+        let pt4 = &mut *pt4_ptr;
+
+        // Clear the first entry. Leaks the level 3 table but we don't care at this point.
+        pt4[0].clear();
+
+        // Flush the TLB.
+        asm!("movq %cr3, %rax; movq %rax, %cr3" : : : "rax");
+    }
+
+    // Initialize the console.
     framebuffer.fill(0, 0, 0);
     let mut console = Console::new(framebuffer);
 
